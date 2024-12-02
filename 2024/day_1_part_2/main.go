ba73f2e061eeee2b9262ca48583a06608818a899
Rupert Hammond <ruperthammond@costellomedical.com>
2024-12-02 14:47:01 +0000

    Quality of life changes and debugging

--- 2024/day_1_part_2/main.go
+++ 2024/day_1_part_2/main.go
@@ -1,64 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
 )
 
-/*
-*
-
-	Calculate a similarity score by adding u[ rach number in the left list
-	after multiplying it by the number of times that number appears in the right list.
-
-*
-*/
 func main() {
-	input_file, _ := os.Open("day_1_input.txt")
-	similarityScore := 0
+	left_list, right_list := ReadChallengeInput("day_1_input.txt")
 
-	// Annon Cleanup function to close file after use.
-	defer func() {
-		err := input_file.Close()
-		if err != nil {
-			panic(err)
-		}
-	}()
+	sort.Ints(left_list)
+	sort.Ints(right_list)
 
-	for {
-		input_data := make([]byte, 1024)
-		n, err := input_file.Read(input_data) // Big bang file read (Is that the best way of doing it?)
+	fmt.Printf("Total Similarity: %v\n", calculateSimilarityScore(left_list, right_list))
+}
 
-		if err != nil && err != io.EOF {
-			panic(err)
-		} else if n == 0 {
-			break
-		}
-
-		inputString := strings.Trim(string(input_data[:]), "\n") // Get rid of any trailing characters
-		sa := strings.Split(inputString, "\n")
-
-		group1 := make([]int, len(sa)/2)
-		group2 := make([]int, len(sa)/2)
-
-		for _, pair := range sa {
-			pair2Assign := strings.Split(pair, " ")
-			group1 = append(group1, extractInt(pair2Assign[0]))
-			group2 = append(group2, extractInt(pair2Assign[1]))
-		}
-
-		sort.Ints(group1)
-		sort.Ints(group2)
-
-		similarityScore += calculateSimilarityScore(group1, group2)
+/*
+Accept a filepath input, assuming each record within the file is
+delimetered with the newline character '\n', then return the parsed left and
+right hand integer list
+*/
+func ReadChallengeInput(filepath string) (left_list, right_list []int) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
 	}
+	defer file.Close()
 
-	fmt.Printf("Total Similarity: %v\n", similarityScore)
+	scanner := bufio.NewScanner(file)
 
+	for scanner.Scan() {
+		pair := strings.Split(scanner.Text(), " ")
+		left_list = append(left_list, extractInt(pair[0]))
+		right_list = append(right_list, extractInt(pair[1]))
+	}
+	return
 }
 
 func extractInt(s string) int {
@@ -75,8 +54,8 @@ func calculateSimilarityScore(left_list, right_list []int) int {
 	score := 0
 
 	for _, item_left := range left_list {
-
-		occurance_map[item_left] += perform_modified_binary_search(item_left, right_list[:])
+		fmt.Printf("Next instance: %v\n", item_left)
+		occurance_map[item_left] += perform_modified_binary_search(item_left, right_list)
 	}
 
 	for key, value := range occurance_map {
@@ -94,18 +73,16 @@ check on either side of the element to see if any more are present, if so, work 
 the direction found element no longer exists (counting each one) then terminate
 */
 func perform_modified_binary_search(element int, search_space []int) int {
-
 	if len(search_space) == 0 {
 		return 0 // handle the case where no occurences found
 	}
 
 	m := len(search_space) / 2
 
-	current_element := search_space[m]
-	if current_element < element {
+	if search_space[m] < element {
 		perform_modified_binary_search(element, search_space[m:])
-	} else if current_element > element {
-		perform_modified_binary_search(element, search_space[0:m])
+	} else if search_space[m] > element {
+		perform_modified_binary_search(element, search_space[:m])
 	} else {
 		occurences := 1
 
@@ -118,6 +95,5 @@ func perform_modified_binary_search(element int, search_space []int) int {
 		}
 		return occurences
 	}
-
 	return 0 // shouldn't reach this.
 }

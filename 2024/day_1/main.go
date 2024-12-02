4e20fdd5ffa397da9ef216614ac83b7910f1ef9a
Rupert Hammond <ruperthammond@costellomedical.com>
2024-12-02 10:59:41 +0000

    finished part one of day_1

--- 2024/day_1/main.go
+++ 2024/day_1/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 /*
@@ -34,6 +38,29 @@ func main() {
 		fmt.Println("File read successfully")
 	}
 
+	inputString := strings.Trim(string(input_data[:]), "\n") // Get rid of any trailing characters
+	sa := strings.Split(inputString, "\n")
+
+	group1 := make([]int, len(sa)/2)
+	group2 := make([]int, len(sa)/2)
+
+	for _, pair := range sa {
+		pair2Assign := strings.Split(pair, " ")
+		group1 = append(group1, extractInt(pair2Assign[0]))
+		group2 = append(group2, extractInt(pair2Assign[1]))
+	}
+
+	sort.Ints(group1)
+	sort.Ints(group2)
+
+	sum := 0.0
+
+	for i := 0; i < len(group1); i++ {
+		sum += math.Abs(float64(group1[i] - group2[i]))
+	}
+
+	fmt.Printf("Total difference: %f\n", sum)
+
 	// Annon Cleanup function to close file after use.
 	defer func() {
 		err := input_file.Close()
@@ -41,4 +68,12 @@ func main() {
 			panic(err)
 		}
 	}()
+}
+
+func extractInt(s string) int {
+	i, err := strconv.ParseInt(s, 10, 0)
+	if err != nil {
+		os.Exit(-1)
+	}
+	return int(i)
 }

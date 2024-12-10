a7b9e7c9871e41526627fd751ffef16805bd0c54
Rupert Hammond <ruperthammond@costellomedical.com>
2024-12-10 16:14:37 +0000

    added in a few more notes about day four

--- 2024/day_4/part_1/main.go
+++ 2024/day_4/part_1/main.go
@@ -116,29 +116,46 @@ func (vs *VerticalSearch) FindTermOccurrences(ws *WordSearch) (occurrences int) 
 }
 
 /*
-let i = index into column
-let j = index into row
-starting in the bottom left hand corner of the array, i == len(ws.SearchSpace) - len(ws.SearchTerm)
-set j = 0
+Diagonal Search is a little more tricky that the others,
+We want to move across the diagonal lines, however we have to exclude all
+whose length (number of dicrete points n) < len(ws.SearchTerm)
 
-	iterate up until j = ub
+This seems like we've maybe got to do a little pre-processing. One thing we can
+do is build up a slice containing the indexes of the diagonals [][]int
 
-decrement i
+[
+
+	[3,0], (i, j)
+	[2,1],
+	[1,2],
+	[0,3]
+
+]
+
+We take the size of the diagonals len(diagonals) and remove anything whose size is
+< len(ws.SearchTerm)
+
+Now Iterate over the remaining lines with the sliding window, use this to extract out
+a string compare it to the desired searchTerm.
+
+Feels like I shouldn't have to do this preprocessing stage though... Feels pretty annoying,
+I'd prefer to just do the work as part of the original loop.
 */
 func (ds *DiagonalSearch) FindTermOccurrences(ws *WordSearch) (occurrences int) {
+	var diagonals [][]int
 
-	for k := 0; k < len(ws.SearchSpace)-len(ws.SearchTerm); k++ {
+	for k := len(ws.SearchTerm) - 1; k < len(ws.SearchSpace); k++ {
+		offset := len(ws.SearchTerm)
 		for j := 0; j <= k; j++ {
 			i := k - j
-			fmt.Print(string(ws.SearchSpace[i][j]))
+			// ws.SearchSpace[i][j])
 		}
-		fmt.Println()
 	}
 
-	for k := len(ws.SearchSpace) - len(ws.SearchTerm); k >= 0; k-- {
+	for k := len(ws.SearchSpace); k > len(ws.SearchTerm)-1; k-- {
 		for j := 0; j <= k; j++ {
 			i := k - j
-			fmt.Print(string(ws.SearchSpace[i][j]))
+			// ws.SearchSpace[i][j])
 		}
 		fmt.Println()
 	}

c25517dc7d08d6aab30253b93aa9fb56d557422d
Rupert Hammond <ruperthammond@costellomedical.com>
2024-12-12 09:42:36 +0000

    brute forcing for the win

--- 2024/day_4/part_1_take_2/main.go
+++ 2024/day_4/part_1_take_2/main.go
@@ -8,19 +8,19 @@ import (
 )
 
 func main() {
-	//	input := ReadChallengeInput("../day_4_input.txt")
-	input := []string{
-		"MMMSXXMASM",
-		"MSAMXMSMSA",
-		"AMXSXMAAMM",
-		"MSAMASMSMX",
-		"XMASAMXAMM",
-		"XXAMMXXAMA",
-		"SMSMSASXSS",
-		"SAXAMASAAA",
-		"MAMMMXMMMM",
-		"MXMXAXMASX",
-	}
+	input := ReadChallengeInput("../day_4_input.txt")
+	//	input := []string{
+	//		"MMMSXXMASM",
+	//		"MSAMXMSMSA",
+	//		"AMXSXMAAMM",
+	//		"MSAMASMSMX",
+	//		"XMASAMXAMM",
+	//		"XXAMMXXAMA",
+	//		"SMSMSASXSS",
+	//		"SAXAMASAAA",
+	//		"MAMMMXMMMM",
+	//		"MXMXAXMASX",
+	//	}
 
 	ans := 0
 
@@ -37,7 +37,7 @@ func main() {
 			}
 
 			// Forwards Horizontal Case
-			if j < len(input)-2 && input[i][j+1] == 'M' && input[i][j+2] == 'A' && input[i][j+3] == 'S' {
+			if j < len(input)-3 && input[i][j+1] == 'M' && input[i][j+2] == 'A' && input[i][j+3] == 'S' {
 				ans += 1
 			}
 
@@ -47,7 +47,7 @@ func main() {
 			}
 
 			// Backward vertical Case
-			if i < len(input)-2 && input[i+1][j] == 'M' && input[i+2][j] == 'A' && input[i+3][j] == 'S' {
+			if i < len(input)-3 && input[i+1][j] == 'M' && input[i+2][j] == 'A' && input[i+3][j] == 'S' {
 				ans += 1
 			}
 
@@ -57,17 +57,17 @@ func main() {
 			}
 
 			// top right-diagonal
-			if i > 2 && j < len(input)-2 && input[i-1][j+1] == 'M' && input[i-2][j+2] == 'A' && input[i-3][j+3] == 'S' {
+			if i > 2 && j < len(input)-3 && input[i-1][j+1] == 'M' && input[i-2][j+2] == 'A' && input[i-3][j+3] == 'S' {
 				ans += 1
 			}
 
 			// bottom right-diagonal
-			if i < len(input)-2 && j < len(input)-2 && input[i+1][j+1] == 'M' && input[i+2][j+2] == 'A' && input[i+3][j+3] == 'S' {
+			if i < len(input)-3 && j < len(input)-3 && input[i+1][j+1] == 'M' && input[i+2][j+2] == 'A' && input[i+3][j+3] == 'S' {
 				ans += 1
 			}
 
 			// bottom left-diagonal
-			if i < len(input)-2 && j > 2 && input[i+1][j-1] == 'M' && input[i+2][j-2] == 'A' && input[i+3][j-3] == 'S' {
+			if i < len(input)-3 && j > 2 && input[i+1][j-1] == 'M' && input[i+2][j-2] == 'A' && input[i+3][j-3] == 'S' {
 				ans += 1
 			}
 

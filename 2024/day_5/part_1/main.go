b4e0f6a7b57e7ae90f0cbe6cd04afc0dbd20370c
Rupert Hammond <ruperthammond@costellomedical.com>
2024-12-12 15:56:55 +0000

    give this a rest now, feeling braindead

--- 2024/day_5/part_1/main.go
+++ 2024/day_5/part_1/main.go
@@ -7,18 +7,60 @@ import (
 	"strconv"
 )
 
+/*
+The output should just be the sum of the middle values
+of the rows that are determined to be valid!
+*/
 func main() {
-	update_instructions := ReadChallengeInput("../page_ordering_rules.txt")
+	// update_instructions := ReadChallengeInput("../page_ordering_rules.txt")
 	// ordering_rules := ReadChallengeInput("../update_orders.txt")
-	priority_map := make(map[int][]int, len(update_instructions))
+	ordering_rules := []string{
+		"47|53",
+		"97|13",
+		"97|61",
+		"97|47",
+		"75|29",
+		"61|13",
+		"75|53",
+		"29|13",
+		"97|29",
+		"53|29",
+		"61|53",
+		"97|53",
+		"61|29",
+		"47|13",
+		"75|47",
+		"97|75",
+		"47|61",
+		"75|61",
+		"47|29",
+		"75|13",
+		"53|13",
+	}
+
+	update_instructions := []string{
+		"75,47,61,53,29",
+		"97,61,53,29,13",
+		"75,29,13",
+		"75,97,47,61,53",
+		"61,13,29",
+		"97,13,75,29,47",
+	}
+
+	priority_map := make(map[int][]int, len(ordering_rules))
 
 	// populate the map
-	for _, instruction := range update_instructions {
-		before := extractInt(instruction[0:2])
-		after := extractInt(instruction[3:5])
+	for _, rule := range ordering_rules {
+		before := extractInt(rule[0:2])
+		after := extractInt(rule[3:5])
 
-		priority_map[before] = append(priority_map[before], after)
-		fmt.Printf("key %v, map: %v\n", before, priority_map[before])
+		priority_map[after] = append(priority_map[after], before)
+	}
+
+	fmt.Printf("final map: %v\n", priority_map)
+
+	for _, instruction := range update_instructions {
+		fmt.Println(instruction)
 	}
 }
 

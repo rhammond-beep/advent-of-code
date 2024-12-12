7549f47798a8c0f47f0f02652ff2e3eee13abdcb
Rupert Hammond <ruperthammond@costellomedical.com>
2024-12-12 09:33:30 +0000

    added diagonal cases

--- 2024/day_4/part_1_take_2/main.go
+++ 2024/day_4/part_1_take_2/main.go
@@ -8,8 +8,19 @@ import (
 )
 
 func main() {
-	input := ReadChallengeInput("../day_4_input.txt")
-	targetSet := []string{"M", "A", "S"}
+	//	input := ReadChallengeInput("../day_4_input.txt")
+	input := []string{
+		"MMMSXXMASM",
+		"MSAMXMSMSA",
+		"AMXSXMAAMM",
+		"MSAMASMSMX",
+		"XMASAMXAMM",
+		"XXAMMXXAMA",
+		"SMSMSASXSS",
+		"SAXAMASAAA",
+		"MAMMMXMMMM",
+		"MXMXAXMASX",
+	}
 
 	ans := 0
 
@@ -20,11 +31,43 @@ func main() {
 				continue
 			}
 
+			// Backwards Horizontal Case
 			if j > 2 && input[i][j-1] == 'M' && input[i][j-2] == 'A' && input[i][j-3] == 'S' {
 				ans += 1
 			}
 
+			// Forwards Horizontal Case
+			if j < len(input)-2 && input[i][j+1] == 'M' && input[i][j+2] == 'A' && input[i][j+3] == 'S' {
+				ans += 1
+			}
+
+			// Forward vertical Case
 			if i > 2 && input[i-1][j] == 'M' && input[i-2][j] == 'A' && input[i-3][j] == 'S' {
+				ans += 1
+			}
+
+			// Backward vertical Case
+			if i < len(input)-2 && input[i+1][j] == 'M' && input[i+2][j] == 'A' && input[i+3][j] == 'S' {
+				ans += 1
+			}
+
+			// top left-diagonal
+			if i > 2 && j > 2 && input[i-1][j-1] == 'M' && input[i-2][j-2] == 'A' && input[i-3][j-3] == 'S' {
+				ans += 1
+			}
+
+			// top right-diagonal
+			if i > 2 && j < len(input)-2 && input[i-1][j+1] == 'M' && input[i-2][j+2] == 'A' && input[i-3][j+3] == 'S' {
+				ans += 1
+			}
+
+			// bottom right-diagonal
+			if i < len(input)-2 && j < len(input)-2 && input[i+1][j+1] == 'M' && input[i+2][j+2] == 'A' && input[i+3][j+3] == 'S' {
+				ans += 1
+			}
+
+			// bottom left-diagonal
+			if i < len(input)-2 && j > 2 && input[i+1][j-1] == 'M' && input[i+2][j-2] == 'A' && input[i+3][j-3] == 'S' {
 				ans += 1
 			}
 

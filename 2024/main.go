7f83ff0b47fe1cbda9376a9cded74fb881c83c7b
Rupert Hammond <ruperthammond@costellomedical.com>
2025-01-04 10:32:30 +0000

    some ideas around calculating where the incident entitiy should go

--- 2024/main.go
+++ 2024/main.go
@@ -21,16 +21,36 @@ func main() {
 	lab := day6.BuildMap(input)
 	graph := day6.CreateEmptyGraphFromMap(&lab)
 	graph.CreateEdges(&lab)
-	graph.PrintGraphVisulisation()
+
+	lab.ObstacleLocations[*lab.GuardLocation] = true // ignore the guard's starting position
+
+	for point, obstaclePresent := range lab.ObstacleLocations { // For every viable location
+		if obstaclePresent { // Then a node already exists, so move on.
+			continue
+		}
+
+		newObstacle := &day6.Node{Position: point, Type: "obstacle", Edges: make([]*day6.Edge, 0)} // Create the new obstacle
+
+		// try to Knit into graph based on surronding obstacles and guard's trail through the lab
+		for node_point, node := range graph.Nodes {
+			edge := &day6.Edge{Node: newObstacle}
+
+			if node_point.X == (point.X + 1) {
+				_, err := node.Walk("north")
+				if node_point.Y > point.Y && err != nil {
+					edge.Direction = "east" // In the case where the new obstacle lies above the existing obstacle, the guard must be going east and is to the right of it, the guard must be going east
+					node.Edges = append(node.Edges, edge)
+				} else if node_point.Y < point.Y { //
+					edge.Direction = "west"
+					node.Edges = append(node.Edges, edge)
+				}
+
+			}
+
+		}
+
+	}
 
 	// _, nodes, error := graph.WalkGraphFromNode(day6.Point{X: 6, Y: 4}, day6.Point{X: 3, Y: 2}, "north")
-
-	// if error != nil {
-	// 	fmt.Printf("Error Encountered while walking graph: %v", error.Error())
-	// }
-
-	//for i, node := range nodes {
-	//	fmt.Printf("Order Visited: %v Node Value: %v\n", i+1, node)
-	//}
 
 }

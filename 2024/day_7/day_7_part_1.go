2650b54e087d2557e7b0c2f08fef20a3998638cc
Rupert Hammond <ruperthammond@costellomedical.com>
2025-01-05 20:33:30 +0000

    I think i've come up with something that makes sense, a map which, for a given index, returns the equations to evaluate for that position

--- 2024/day_7/day_7_part_1.go
+++ 2024/day_7/day_7_part_1.go
@@ -34,6 +34,7 @@ func SolveDay7Part1() int {
 		for _, operand := range strings.Split(operands, " ") {
 			Operands = append(Operands, helper.ExtractInt(operand))
 		}
+
 		equation := &Equation{Result: helper.ExtractInt(result), Operands: Operands, Operators: []rune{'+', '*'}}
 
 		if equation.DoesEvaluateToResult() {
@@ -53,6 +54,55 @@ type Equation struct {
 	Operators []rune
 }
 
+type Expression struct {
+	Left     int
+	Right    int
+	Operator rune
+}
+
+/*
+Make all of the possible expressions for a given input string
+returns a slice containing all of the potential expressions in order I:E -
+
+input: operands = [1 2 3]
+
+output:
+
+	{
+		1 -> &{Left: 1, Right: 2, Operator: '+'}, &{Left: 1, Right: 2, Operator: '*'}
+		2 -> &{Left: 2, Right: 3, Operator: '+'}, &{Left: 2, Right: 3, Operator: '*'}
+	}
+
+But then this doesn't get around the issue of making sure we evalue things in order.
+
+We need to test every possible combination
+*/
+func constructExpressions(operands []int) map[int][]Expression {
+	expressionsOrderMap := make(map[int][]Expression)
+
+	for i := 1; i < len(operands)-1; i++ {
+		expressions := make([]Expression, 0, 2)
+		expressions = append(expressions, Expression{Left: operands[i-1], Right: operands[i], Operator: '+'})
+		expressions = append(expressions, Expression{Left: operands[i-1], Right: operands[i], Operator: '*'})
+		expressionsOrderMap[i-1] = expressions
+	}
+
+	return expressionsOrderMap
+}
+
+func (e *Expression) Evaluate() int {
+
+	if e.Operator == '+' {
+		return e.Left + e.Right
+	}
+
+	if e.Operator == '*' {
+		return e.Left * e.Right
+	}
+
+	panic("we really shouldn't be here")
+}
+
 /*
 It can be any combination of additions and multiplys!!
 
@@ -69,9 +119,9 @@ func (e *Equation) DoesEvaluateToResult() bool {
 	// construct the set containing all possible permutations of n elements, where n = operand_positions
 	positionMap := make(map[int][]rune)
 
-	for i := 0; i <= operand_positions; i++ {
-
-	}
+	// cycle through possible combinations of the Operators (each operator needs a go in a given position)
+	// if you get a match then return true
+	// otherwise return false
 
 	return false
 }

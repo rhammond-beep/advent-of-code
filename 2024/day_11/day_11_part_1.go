e5ae7e1a7d54277a88d77170bf3994e2f4496463
Rupert Hammond <ruperthammond@costellomedical.com>
2025-01-22 10:02:44 +0000

    cracked on a little more in the morning

--- 2024/day_11/day_11_part_1.go
+++ 2024/day_11/day_11_part_1.go
@@ -1,6 +1,9 @@
 package day11
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 /*
 Plutonian Pebbles, every time i blink the number on the the stones change.
@@ -13,7 +16,8 @@ no matter how the stones change, their order is preserved, and they can stay on 
 */
 func SolveDay11Part1() {
 	var pp Plutonian
-	puzzleInput := []int64{0, 1, 10, 99, 999}
+	puzzleInput := []int{0, 1, 10, 99, 999}
+	// puzzleInput := []int64{70949 6183 4 3825336 613971 0 15 182}
 	pp = &Pebble{Line: puzzleInput}
 
 	for i := 0; i < 1; i++ {
@@ -24,7 +28,7 @@ func SolveDay11Part1() {
 }
 
 type Pebble struct {
-	Line []int64
+	Line []int
 }
 
 /*
@@ -37,12 +41,21 @@ func (p *Pebble) blink() {
 		if pebble == 0 {
 			p.Line[i] = 1
 		}
-		if true { // even number of digits
+		if isEven, stringRep := checkEvenDigits(pebble); isEven {
+			n := len(stringRep)
+			leftStone, _ := strconv.ParseInt(stringRep[:n], 10, 64)
+			rightStone, _ := strconv.ParseInt(stringRep[n:], 10, 64)
+			// shift array to the left and right
 
 		} else {
 			p.Line[i] = pebble * 2024
 		}
 	}
+}
+
+func checkEvenDigits(digits int) (bool, string) {
+	stringRep := strconv.Itoa(digits)
+	return len(stringRep)%2 == 0, stringRep
 }
 
 func (p *Pebble) stones() int {

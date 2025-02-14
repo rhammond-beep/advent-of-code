7bf7c76360b6bef045a2ff7a7b9289142b559974
Rupert Hammond <ruperthammond@costellomedical.com>
2025-02-14 15:29:46 +0000

    notes on day 12

---
+++ 2024/day_12/day_12_part_1.go
@@ -0,0 +1,14 @@
+package day_12
+
+/*
+Gardening Group Fun
+
+Calculate the total price of the fencing between specific groups which is defined as: "PA", where P = perimeter and A = area
+
+To make this somewhat more complicated, it is perfectly possible to have nested regions, assuming a plabt of a given type is completely enclosed within some given outer region.
+
+If this is the case, the outer region's perimeter must include the sum of it's inner boundaries. (This feels really recursive)
+*/
+func SolveDay12Part1() {
+
+}
